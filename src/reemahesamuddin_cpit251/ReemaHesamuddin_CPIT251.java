487849644373a64cbd339fcc587d3920a70b38e8
Reema Hesamuddin <rabdullahhesamudin@stu.kau.edu.sa>
2023-09-05 12:55:03 +0300

    First Commmit!

---
+++ src/reemahesamuddin_cpit251/ReemaHesamuddin_CPIT251.java
@@ -0,0 +1,15 @@
+/*
+ * Name : Reema Samir Hesamuddin
+ * ID: 2106126
+ * Course:CPIT-251 
+ * Section: AA8
+ */
+
+package reemahesamuddin_cpit251;
+
+public class ReemaHesamuddin_CPIT251 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+    
+}
